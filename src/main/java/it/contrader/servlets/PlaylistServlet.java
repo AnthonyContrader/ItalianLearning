c76d5e88505f7404f4e15c825d3b493d9aff7159
SandrosFist <alessandroalfieri95@gmail.com>
2020-04-20 10:26:20 +0200

    Fixes Playlist

--- src/main/java/it/contrader/servlets/PlaylistServlet.java
+++ src/main/java/it/contrader/servlets/PlaylistServlet.java
@@ -57,7 +57,7 @@ public class PlaylistServlet extends HttpServlet {
 			l.put("solution", fm.getSolution());
 			l.put("typeGame", FindMistakeDTO.getTypeGame());
 			l.put("name", "Find Mistake");
-			l.put("checked", "" + service.find(Integer.parseInt(request.getParameter("id")), fm.getId(), fm.getTypeGame()));
+			l.put("checked", "" + service.find(Integer.parseInt(request.getParameter("id")), fm.getId(), FindMistakeDTO.getTypeGame()));
 			arr.add(l);
 		}
 		gameList.put(FindMistakeDTO.getTypeGame(), arr);
@@ -72,7 +72,7 @@ public class PlaylistServlet extends HttpServlet {
 			l.put("solution", gp.getSolution());
 			l.put("typeGame", GuessPictureDTO.getTypeGame());
 			l.put("name", "Guess Picture");
-			l.put("checked", "" + service.find(Integer.parseInt(request.getParameter("id")), gp.getId(), gp.getTypeGame()));
+			l.put("checked", "" + service.find(Integer.parseInt(request.getParameter("id")), gp.getId(), GuessPictureDTO.getTypeGame()));
 			arr.add(l);
 		}
 		gameList.put(GuessPictureDTO.getTypeGame(), arr);
@@ -86,7 +86,7 @@ public class PlaylistServlet extends HttpServlet {
 			l.put("solution", h.getSolution());
 			l.put("typeGame", HangmanDTO.getTypeGame());
 			l.put("name", HangmanDTO.getTypeGame());
-			l.put("checked", "" + service.find(Integer.parseInt(request.getParameter("id")), h.getId(), h.getTypeGame()));
+			l.put("checked", "" + service.find(Integer.parseInt(request.getParameter("id")), h.getId(), HangmanDTO.getTypeGame()));
 			arr.add(l);
 		}
 		gameList.put(HangmanDTO.getTypeGame(), arr);
@@ -100,7 +100,7 @@ public class PlaylistServlet extends HttpServlet {
 			l.put("solution", os.getSolution());
 			l.put("typeGame", OrganizeSentenceDTO.getTypeGame());
 			l.put("name", "Organize Sentence");
-			l.put("checked", "" + service.find(Integer.parseInt(request.getParameter("id")), os.getId(), os.getTypeGame()));
+			l.put("checked", "" + service.find(Integer.parseInt(request.getParameter("id")), os.getId(), OrganizeSentenceDTO.getTypeGame()));
 			arr.add(l);
 		}
 		gameList.put(OrganizeSentenceDTO.getTypeGame(), arr);
@@ -114,7 +114,7 @@ public class PlaylistServlet extends HttpServlet {
 			l.put("solution", q.getSolution());
 			l.put("typeGame", QuizDTO.getTypeGame());
 			l.put("name", QuizDTO.getTypeGame());
-			l.put("checked", "" + service.find(Integer.parseInt(request.getParameter("id")), q.getId(), q.getTypeGame()));
+			l.put("checked", "" + service.find(Integer.parseInt(request.getParameter("id")), q.getId(), QuizDTO.getTypeGame()));
 			arr.add(l);
 		}
 		gameList.put(QuizDTO.getTypeGame(), arr);
