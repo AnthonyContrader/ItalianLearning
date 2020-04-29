806cc93b5779cf1c18c5301e05f4dcbf1a42560c
Mario <Muscio-1605187@users.noreply.github.com>
2020-04-29 21:24:14 +0200

    front end layout

--- src/app/admin/quiz/quiz.component.ts
+++ src/app/admin/quiz/quiz.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizDTO } from 'src/dto/quizdto';
 import {QuizService} from 'src/service/quiz.service';
+import { LevelDTO } from 'src/dto/leveldto';
+import { CategoryDTO } from 'src/dto/categorydto';
+import { CategoryService } from 'src/service/category.service';
+import { LevelService } from 'src/service/level.service';
 
 /* created by Anna Cecere*/
 
@@ -13,15 +17,25 @@ export class QuizComponent implements OnInit {
   quizDTO: QuizDTO[];
   quiztoinsert : QuizDTO = new QuizDTO();
 
-  constructor(private service: QuizService) { }
+  levelsDTO: LevelDTO[];
+  categoriesDTO: CategoryDTO[];
+
+  constructor(private service: QuizService, private serviceCategory: CategoryService, private serviceLevel:LevelService) { }
 
   ngOnInit() {
     this.getQuiz();
-   
+    this.getCategories();
+    this.getLevels();
     }
   getQuiz() {
     this.service.getAll().subscribe(quiz => this.quizDTO = quiz); 
     //l'oggetto observable tramite subscribe ci passa i dati con parametro da lista game che verrà salvato nella varibile dto
+  }
+  getLevels() {
+    this.serviceLevel.getAll().subscribe(level => this.levelsDTO = level);
+  }
+  getCategories() {
+    this.serviceCategory.getAll().subscribe(category => this.categoriesDTO = category);
   }
   delete(quiz: QuizDTO) {
     this.service.delete(quiz.id).subscribe(() => this.getQuiz());
