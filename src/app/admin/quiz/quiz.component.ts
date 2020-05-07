0c6e5447b41d3b526fa422753721543c494913d6
Mario <Muscio-1605187@users.noreply.github.com>
2020-05-07 18:29:21 +0200

    modifiche di quiz

--- src/app/admin/quiz/quiz.component.ts
+++ src/app/admin/quiz/quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { QuizDTO } from 'src/dto/quizdto';
 import {QuizService} from 'src/service/quiz.service';
 import { LevelDTO } from 'src/dto/leveldto';
@@ -14,20 +14,23 @@ import { LevelService } from 'src/service/level.service';
   styleUrls: ['./quiz.component.css']
 })
 export class QuizComponent implements OnInit {
-  quizDTO: QuizDTO[];
+  quizzesDTO: QuizDTO[];
   levelsDTO: LevelDTO[];
   categoriesDTO: CategoryDTO[];
   quiztoinsert : QuizDTO = new QuizDTO();
+  @ViewChild('newQuizForm') quizForm;
+  @ViewChild('modalTitle') modalTitle;
+  @ViewChild('newModal') newModal;
 
   constructor(private service: QuizService, private serviceCategory: CategoryService, private serviceLevel:LevelService) { }
 
   ngOnInit() {
-    this.getQuiz();
+    this.getQuizzes();
     this.getCategories();
     this.getLevels();
     }
-  getQuiz() {
-    this.service.getAll().subscribe(quiz => this.quizDTO = quiz); 
+  getQuizzes() {
+    this.service.getAll().subscribe(quiz => this.quizzesDTO = quiz); 
     //l'oggetto observable tramite subscribe ci passa i dati con parametro da lista game che verrà salvato nella varibile dto
     }
   getLevels() {
@@ -37,20 +40,36 @@ export class QuizComponent implements OnInit {
     this.serviceCategory.getAll().subscribe(category => this.categoriesDTO = category);
   }
   delete(quiz: QuizDTO) {
-    this.service.delete(quiz.id).subscribe(() => this.getQuiz());
+    this.service.delete(quiz.id).subscribe(() => this.getQuizzes());
     //cancella i dati e restituisce la lista aggiornata
   }
+  editQuiz(quiz: QuizDTO){
+    this.quizForm.reset()
+    if(quiz != null){
+      this.service.read(quiz.id).subscribe(quiz => this.quiztoinsert = quiz);
+      this.modalTitle.nativeElement.textContent = 'Edit Quiz ' + quiz.id
+    }
+    else
+      this.modalTitle.nativeElement.textContent = 'New Quiz'
+  }
   update(quiz: QuizDTO) {
-    this.service.update(quiz).subscribe(() => this.getQuiz());
+    this.service.update(quiz).subscribe(() => this.getQuizzes());
     
     //aggiorna i dati e restituisce la lista aggiornata
   }
   insert(quiz: QuizDTO){
     console.log(quiz);
-    this.service.insert(quiz).subscribe(() => this.getQuiz());
+    this.service.insert(quiz).subscribe(() => this.getQuizzes());
     this.clear();
   }
   clear(){
     this.quiztoinsert = new QuizDTO();
-      }
+  }
+  onSubmit(quiz: QuizDTO) {
+    if (quiz.id != null)
+      this.update(quiz)
+    else
+      this.insert(quiz)
+  }
+    
 }
