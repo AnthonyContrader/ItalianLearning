bb909512c07a9e30736b6b938eb749e8982c928c
Mario <Muscio-1605187@users.noreply.github.com>
2020-05-08 12:24:17 +0200

    quiz mod

--- src/app/admin/quiz/quiz.component.spec.ts
+++ src/app/admin/quiz/quiz.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { QuizComponent } from './quiz.component';
+/*created by Anna Cecere */
 
 describe('QuizComponent', () => {
   let component: QuizComponent;
