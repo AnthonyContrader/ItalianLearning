42f2941e70c79641bf3ebfa2202c048bffb1876e
Enzo Tasca <enzotasca.et@gmail.com>
2020-05-09 15:42:29 +0200

    fix insert AccountService

--- src/service/accountservice.ts
+++ src/service/accountservice.ts
@@ -2,6 +2,7 @@ import { AbstractService } from './abstractservice';
 import { UserDTO } from 'src/dto/userdto';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -10,9 +11,12 @@ import { HttpClient } from '@angular/common/http';
     constructor(http: HttpClient) {
       super(http);
       this.type = 'accounts';
-      this.micro = 'login';
+      this.micro = '';
     }
-  
+
+    insert(dto: UserDTO): Observable<any> {
+      return this.http.post('http://localhost:' + this.port + '/' + this.type + '/insert', dto);
+    }
   }
 
 
